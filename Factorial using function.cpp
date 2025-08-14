3a6adc38364a8fd1596446b86d166bbecb050b13
Amir Hossain Limon <146566739+Amirhossainlimon@users.noreply.github.com>
2025-08-15 01:36:45 +0600

    Add files via upload

---
+++ Factorial using function.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+   int n;
+   cout<<"Enter number: ";
+   cin>>n;
+  int factorial=1;
+  for(int i=1; i<=n;i++){
+    factorial*=i;
+ }
+   cout<<"Factorial is "<<factorial<<endl;
+    return 0;
+}
