8cab1d406091aa8fddef1cf0f4858ae4beb2684f
Amir Hossain Limon <146566739+Amirhossainlimon@users.noreply.github.com>
2025-08-15 01:39:04 +0600

    Add files via upload

---
+++ Function.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+bool isprime(int n){
+
+for(int i=2;i<n;i++){
+
+    if(n%i==0){
+        return false;
+    }
+}
+
+  return true;
+}
+
+int main(){
+ int a,b;
+ cin>>a>>b;
+cout<<"The prime numbers between "<<a<<" to "<<b<<" are:"<<endl;
+for(int i=a;i<=b;i++)
+{
+    if(isprime(i)){
+        cout<<i<<endl;
+    }
+
+}
+return 0;
+}
+
